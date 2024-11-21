37506d7a1275b898b3db5c1da1847f7fec007fa3
whoajaydutech <whoajay.dutech@gmail.com>
2024-11-21 14:43:57 +0530

    Create serverawake.js

---
+++ serverawake.js
@@ -0,0 +1,10 @@
+import https from "https";
+
+const keepAwake = () => {
+  https.get("https://blogappbackend-dzi9.onrender.com/api/blogs", (res) => {
+    console.log(`Request sent to keep the server awake: ${res.statusCode}`);
+  });
+};
+
+// Send a request every 28 minutes
+setInterval(keepAwake, 14 * 60 * 1000);
