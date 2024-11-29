581b15e8c7ebd319164bd5419874f7ceb3287c80
whoajaydutech <whoajay.dutech@gmail.com>
2024-11-29 11:58:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use("/uploads", express.static("uploads")); // Serve uploaded images
 app.use("/api/blogs", blogRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  // .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
