28eba18e8c49715df20eb321e4baf20b2395cebe
whoajaydutech <whoajay.dutech@gmail.com>
2024-12-19 06:44:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const blogRoutes = require("./routes/blogRoutes.js");
-require("./serverawake.js");
+// require("./serverawake.js");
 
 dotenv.config();
 
